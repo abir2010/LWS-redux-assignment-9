c999ee5df53e5c5886cba7c52a608678964d04fc
M.K.Abir <kaiserabir222@gmail.com>
2023-12-28 20:56:45 +0600

    bug fixed

--- src/components/TeamListItem.jsx
+++ src/components/TeamListItem.jsx
@@ -1,19 +1,12 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
-import { useEffect, useState } from "react";
 
 export default function TeamListItem({ member }) {
-  const [display, setDisplay] = useState({});
   const { name, avatar } = member || {};
-  const img = `../assets${avatar}`;
-
-  useEffect(() => {
-    import(/* @vite-ignore */ img).then((img) => setDisplay(img.default));
-  }, [img]);
 
   return (
     <div className="checkbox-container">
-      <img src={display} className="team-avater" />
+      <img src={avatar} className="team-avater" />
       <p className="label">{name}</p>
     </div>
   );
